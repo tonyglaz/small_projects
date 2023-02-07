85912340e426a2249aff95674ae093f6d3723a6b
tonyglaz <tonyglazunov@gmail.com>
2023-02-07 13:48:02 +0300

    working outsid eof contex error fixed (need to add app.app_context().push())

--- flask_with_sqlalch/relationship one-to-many/main.py
+++ flask_with_sqlalch/relationship one-to-many/main.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
+app.app_context().push()
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///dota.db"
 db = SQLAlchemy(app)
 
@@ -26,31 +27,30 @@ class Team(db.Model):
     __tablename__ = "team"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100),unique=True)
+    name = db.Column(db.String(100), unique=True)
 
     players = relationship("Player")
 
 
-@app.route('/')
-def create_app():
-    db.create_all()
+db.create_all()
 
-    team_TS = Team(id=1, name="Team Spirit")
-    player_denis = Player(id=1, name="Denis", age=21, nickname="Larl", position=2, team=team_TS)
-    player_yatoro = Player(id=2, name="Ilya", age=19, nickname="Yatoro", position=1, team=team_TS)
+team_TS = Team(id=1, name="Team Spirit")
+player_denis = Player(id=1, name="Denis", age=21, nickname="Larl", position=2, team=team_TS)
+player_yatoro = Player(id=2, name="Ilya", age=19, nickname="Yatoro", position=1, team=team_TS)
 
-    db.session.add(player_denis)
-    db.session.add(player_yatoro)
+with db.session.begin():
+    db.session.add_all([player_denis, player_yatoro])
 
-    player_nightfall = Player(id=3, name="Egor", age=20, nickname="Nightfall", position=3)
-    player_torontotokyo = Player(id=4, name="Alexander", age=25, nickname="TORONTOTOKYO", position=5)
-    team_BB = Team(id=2, name="BetBoom", players=[player_nightfall, player_torontotokyo])
+player_nightfall = Player(id=3, name="Egor", age=20, nickname="Nightfall", position=3)
+player_torontotokyo = Player(id=4, name="Alexander", age=25, nickname="TORONTOTOKYO", position=5)
+team_BB = Team(id=2, name="BetBoom", players=[player_nightfall, player_torontotokyo])
 
+with db.session.begin():
     db.session.add(team_BB)
 
-    player_with_team = Player.query.get(1)
-    return player_with_team.team.name
-
+# player_with_team = Player.query.get(1)
+# print(player_with_team.team.name)
+# print(player_with_team.name)
 
 if __name__ == "__main__":
     app.run(debug=True)
