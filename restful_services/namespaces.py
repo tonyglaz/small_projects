399ece87420b12c0d438fbbb1afcd841122d597d
tonyglaz <tonyglazunov@gmail.com>
2023-02-07 14:21:11 +0300

    namespaces

--- restful_services/namespaces.py
+++ restful_services/namespaces.py
@@ -14,30 +14,51 @@ class Book(db.Model):
     __tablename__ = 'book'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    author = db.Column(db.String(100))
     year = db.Column(db.Integer)
+
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
 
 
 class BookSchema(Schema):
     id = fields.Int()
     name = fields.Str()
-    author = fields.Str()
     year = fields.Int()
+
+
+class AuthorSchema(Schema):
+    id = fields.Int()
+    first_name = fields.Str()
+    last_name = fields.Str()
+
 
 book_schema = BookSchema()
 books_schema = BookSchema(many=True)
+
+author_schema = AuthorSchema()
+authors_schema = AuthorSchema(many=True)
+
 api = Api(app)
-book_ns = api.namespace('')
-b1 = Book(id=1, name="Solaris", year=1961, author="Stanisław Lem")
-b2 = Book(id=2, name="Roadside picnic", year=1972, author="brothers Strugatsky")
+book_ns = api.namespace('books')
+author_ns = api.namespace('authors')
+
+b1 = Book(id=1, name="Solaris", year=1961)
+b2 = Book(id=2, name="Sisyphus", year=1937)
+
+a1 = Author(id=1, first_name="Stanisław", last_name="Lem")
+a2 = Author(id=2, first_name="Albert", last_name="Camus")
 
 db.create_all()
 
 with db.session.begin():
     db.session.add_all([b1, b2])
+    db.session.add_all([a1, a2])
 
 
-@book_ns.route('/books')
+@book_ns.route('/') # / + books + /
 class BooksView(Resource):
     def get(self):
         all_books = db.session.query(Book).all()
@@ -46,12 +67,13 @@ class BooksView(Resource):
     def post(self):
         req_json = request.json
         new_book = Book(**req_json)
+
         with db.session.begin():
             db.session.add(new_book)
         return "", 201
 
 
-@book_ns.route('/books/<int:bid>')
+@book_ns.route('/<int:bid>')
 class BookView(Resource):
     def get(self, bid: int):  # Получил данные
         try:
@@ -65,7 +87,6 @@ class BookView(Resource):
         req_json = request.json
 
         book.book_name = req_json.get("name")
-        book.book_author = req_json.get("author")
         book.book_year = req_json.get("year")
 
         db.session.add(book)
@@ -78,8 +99,6 @@ class BookView(Resource):
         req_json = request.json
         if "name" in req_json:
             book.name = req_json.get("name")
-        if "author" in req_json:
-            book.name = req_json.get("author")
         if "year" in req_json:
             book.name = req_json.get("year")
 
@@ -90,6 +109,61 @@ class BookView(Resource):
 
     def delete(self, bid: int):
         user = db.session.query(Book).get(bid)
+        db.session.delete(user)
+        db.session.commit()
+
+        return "", 204
+
+
+@author_ns.route('/')
+class AuthorsView(Resource):
+    def get(self):  # Получил данные
+        all_books = db.session.query(Author).all()
+        return authors_schema.dump(all_books), 200
+
+    def post(self):
+        req_json = request.json
+        new_user = Author(**req_json)
+
+        with db.session.begin():
+            db.session.add(new_user)
+
+        return "", 201
+
+
+@author_ns.route('/<int:aid>')
+class AuthorView(Resource):
+    def get(self, aid: int):  # Получил данные
+        author = db.session.query(Author).get(aid)
+        return author_schema.dump(author), 200
+
+    def put(self, aid):  # Заменил данные
+        author = db.session.query(Book).get(aid)
+        req_json = request.json
+
+        author.first_name = req_json.get("first_name")
+        author.last_name = req_json.get("last_name")
+
+        db.session.add(author)
+        db.session.commit()
+
+        return "", 204
+
+    def patch(self, aid):  # частично обновил данные
+        author = db.session.query(Book).get(aid)
+        req_json = request.json
+        if "first_name" in req_json:
+            author.first_name = req_json.get("first_name")
+        if "last_name" in req_json:
+            author.name = req_json.get("last_name")
+
+        db.session.add(author)
+        db.session.commit()
+
+        return "", 204
+
+    def delete(self, aid: int):
+        user = db.session.query(Book).get(aid)
         db.session.delete(user)
         db.session.commit()
 
